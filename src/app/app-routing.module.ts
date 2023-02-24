71c3b875a0df840ed251a2c12f1693f16f87b3c7
Roy Morillo <rmorillor@gmail.com>
2023-02-23 22:29:52 -0400

    Initial Create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "auth",
+    loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
+  },
+  {
+    path: "dashboard",
+    loadChildren: () => import("./protected/protected.module").then(m => m.ProtectedModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: false
+  })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
